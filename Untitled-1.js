b83b7657f3a9e68a0db0298ca89940eba01faab7
Ivory <134550521+blkr4bbits@users.noreply.github.com>
2026-02-09 22:03:23 -0500

    Add files via upload

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,2 +1,30 @@
 //add 3js implementation 
 //make 3d model of myself spinning in a chair when scrolling down on the home page
+
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(window.innerWidth / window.innerHeight );
+renderer.setAnimationLoop( animate );
+document.body.appendChild (renderer.domElement);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setAnimationLoop( animate );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render( scene, camera );
+
+}
